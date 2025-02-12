22213f22aee72936630dcb70092fce25cd2875cf
Dipa-44 <dipaghosh0902@gmail.com>
2025-02-12 22:27:54 +0530

    Validation added

---
+++ contact_form.js
@@ -0,0 +1,29 @@
+// const form=document.querySelector("form");
+
+// const namee=document.getElementById("name");
+// const email=document.getElementById("email");
+// const message=document.getElementById("message");
+
+// function seterror(id, error)
+// {
+//     element=document.getElementById(id);
+//     element.getElementsByClassName('formerror')[0].innerHTML=error;
+// }
+
+// // if(!name || !email || !message) {
+// //     alert("All fileds are mendatory")
+// // }
+
+// function validateForm()
+// {
+//     var returnval=true;
+
+
+//     if(namee.length < 5)
+//     {
+//         seterror("namee", "Length must be greater than 5")
+//     }
+// }
+// form.addEventListener("submit", (e)=>{
+//     e.preventDefault();
+// })
